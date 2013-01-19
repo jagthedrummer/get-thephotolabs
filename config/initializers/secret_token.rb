63c5d261b31ee116be735dd93f38888cfbc68c9a
Jeremy Green <jeremy@octolabs.com>
2013-01-18 23:49:51 -0600

    Getting started on a marketing site for thephotolabs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetThephotolabs::Application.config.secret_token = '7893b06006a51c0664dbc6a1af7f97a3780ea1965aa07a08d7282f0204beef4b1d45b8dd21561d230dd431a4b78683e3eb92c80268d52cf1488a084d7d7234f1'
